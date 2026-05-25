a339ec1d2da06f883bc4b367eca3b132c7976b2f
Vikhom Manpiriya <vikhomman47@gmail.com>
2026-05-25 19:59:36 +0700

    Complete LAB08 Kafka Streaming

---
+++ LAB08-KAFKA-STREAMING/analytics.py
@@ -0,0 +1,7 @@
+sales = [60, 70, 80]
+
+total = 0
+
+for s in sales:
+    total += s
+    print(f"Total Sales: {total}")
---
+++ LAB08-KAFKA-STREAMING/consumer.py
@@ -0,0 +1,8 @@
+orders = [
+    {"order_id": 1, "menu": "Americano", "price": 60},
+    {"order_id": 2, "menu": "Latte", "price": 70},
+    {"order_id": 3, "menu": "Mocha", "price": 80}
+]
+
+for order in orders:
+    print(f"Received: {order}")
